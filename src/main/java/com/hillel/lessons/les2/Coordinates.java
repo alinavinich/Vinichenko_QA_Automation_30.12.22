b46f33f4d4f66199ec15a175a70847fe0b62508b
Alina Vinichenko <popkyavichus@gmail.com>
2022-12-30 11:36:11 +0200

    added all files

---
+++ src/main/java/com/hillel/lessons/les2/Coordinates.java
@@ -0,0 +1,22 @@
+package com.hillel.lessons.les2;
+
+public class Coordinates {
+    public static void main(String[] args) {
+
+        System.out.println("My home coordinates:");
+
+        double latitude = 49.32191553;
+
+        System.out.print("Latitude:");
+        System.out.print(latitude);
+        char symbol = 176;
+        System.out.println(symbol);
+
+        double longitude = 24.10512646;
+
+        System.out.print("Longitude:");
+        System.out.print(longitude);
+        System.out.print(symbol);
+
+    }
+}
