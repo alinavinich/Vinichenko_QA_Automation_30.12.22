4705fc4c3fc303f3876a387bf792e1c2a0872c8b
Alina Vinichenko <popkyavichus@gmail.com>
2023-01-02 16:56:28 +0200

    homework 4.1

---
+++ src/main/java/com/hillel/lessons/les4/Parallelepiped.java
@@ -0,0 +1,17 @@
+package com.hillel.lessons.les4;
+
+public class Parallelepiped {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        int c = 3;
+
+        int volume = a * b * c;
+
+        System.out.println("Volume of parallelepiped = " +volume);
+
+        int length = (a + b +c) * 3;
+
+        System.out.print("Length of sides = " +length);
+    }
+}
